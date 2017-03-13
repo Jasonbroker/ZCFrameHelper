ef1f6aec22fee36b12b397d583c1882b565328a6
Jasonbroker <zhiye1@hotmail.com>
2017-03-13 20:20:02 +0800

    refractOC

---
+++ examples/FrameHelper/FrameHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FrameHelper
+//
+//  Created by Jason on 13/03/2017.
+//  Copyright © 2017 Jason Digital Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ examples/FrameHelper/FrameHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FrameHelper
+//
+//  Created by Jason on 13/03/2017.
+//  Copyright © 2017 Jason Digital Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
